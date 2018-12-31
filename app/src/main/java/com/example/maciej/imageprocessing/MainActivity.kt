43be412e64c06dfd2573109f1ad49feb27a5580e
MCozhusheck <mackozuszek@gmail.com>
2018-12-31 16:58:17 +0100

    camera init

--- app/src/main/java/com/example/maciej/imageprocessing/MainActivity.kt
+++ app/src/main/java/com/example/maciej/imageprocessing/MainActivity.kt
@@ -5,19 +5,55 @@ import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.support.v4.app.FragmentActivity
 import android.support.v4.content.ContextCompat
 import android.util.Log
+import android.view.SurfaceView
+import android.view.WindowManager
 
-class MainActivity : AppCompatActivity() {
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
+
     companion object {
         const val TAG: String = "MainActivity"
         const val PERMISSION_REQUEST_CAMERA: Int = 1
     }
 
+    private var mOpenCvCameraView: CameraBridgeViewBase? = null
+    private val mIsJavaCamera = true
+    var mRgba: Mat? = null
+    var mRgbaF: Mat? = null
+    var mRgbaT: Mat? = null
+    private val mLoaderCallback = object: BaseLoaderCallback(this) {
+        override fun onManagerConnected(status: Int) {
+            when (status) {
+                LoaderCallbackInterface.SUCCESS -> {
+                    Log.i(TAG, "OpenCV loaded successfully")
+                    mOpenCvCameraView?.enableView()
+                }
+                else -> {
+                    super.onManagerConnected(status)
+                }
+            }
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContentView(R.layout.activity_main)
-
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             // Permission is not granted
@@ -30,5 +66,49 @@ class MainActivity : AppCompatActivity() {
         } else {
             Log.i(TAG, "permission granted")
         }
+
+        mOpenCvCameraView = findViewById(R.id.camera);
+        mOpenCvCameraView?.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView?.setCvCameraViewListener(this);
+
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView?.disableView()
+    }
+    public override fun onResume() {
+        super.onResume()
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization")
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback)
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!")
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS)
+        }
+    }
+    public override fun onDestroy() {
+        super.onDestroy()
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView?.disableView()
+
+    }
+    override fun onCameraViewStarted(width: Int, height: Int) {
+        mRgba = Mat(height, width, CvType.CV_8UC4)
+        mRgbaF = Mat(height, width, CvType.CV_8UC4)
+        mRgbaT = Mat(width, width, CvType.CV_8UC4)
+    }
+    override fun onCameraViewStopped() {
+        mRgba?.release();
+    }
+    override fun onCameraFrame(inputFrame: CvCameraViewFrame?): Mat? {
+        mRgba = inputFrame?.rgba();
+        // Rotate mRgba 90 degrees
+//        Core.transpose(mRgba, mRgbaT);
+//        Imgproc.resize(mRgbaT, mRgbaF, mRgbaF?.size(), .0,.0, 0);
+//        Core.flip(mRgbaF, mRgba, 1 );
+
+        return mRgba; // This function must return
     }
 }
